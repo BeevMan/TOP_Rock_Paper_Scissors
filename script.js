045a12f6d8c8b4418047a593fd72fdf2b7356774
Jimmi is Coding <jimmicodathon@gmail.com>
2021-03-18 16:51:23 -0500

    updated with the dom manipulation practice instructions

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let wins = 0;
+let losses = 0;
+let ties = 0;
+let elDisplayHeading = document.getElementsByTagName( "h1" );
+elDisplayHeading = elDisplayHeading[0];
+
 function computerPlay() {
     const zeroToThree = Math.floor(Math.random() * Math.floor(3));
     if ( zeroToThree === 0 ) {
@@ -14,15 +20,51 @@ function oneRound( playerSelection ) {
     playerSelection = capitilize( playerSelection );
     const computerSelection = computerPlay();
     if ( computerSelection === playerSelection ) {
-        return "Tie! Play another"
+        return displayResult( "Tie! Play another round!" )
     }
     else if ( computerSelection === "Rock" ) {
-        return ( playerSelection === "Paper" ? "Paper beats Rock, you win!!!" : "Rock beats Scissors, you lose :(" );
+        return ( playerSelection === "Paper" ? displayResult( "Paper beats Rock, you win!!!" ) : displayResult( "Rock beats Scissors, you lose :(" ) );
     } else if ( computerSelection === "Paper" ) {
-        return ( playerSelection === "Scissors" ? "Scissors beats Paper, you win!!!" : "Paper beats Rock, you lose :(" );
+        return ( playerSelection === "Scissors" ? displayResult( "Scissors beats Paper, you win!!!" ) : displayResult( "Paper beats Rock, you lose :(" ) );
     } else if ( computerSelection === "Scissors" ) {
-        return ( playerSelection === "Rock" ? "Rock beats Scissors, you win!!!" : "Scissors beats Paper, you lose :(" );
+        return ( playerSelection === "Rock" ? displayResult( "Rock beats Scissors, you win!!!" ) : displayResult( "Scissors beats Paper, you lose :(" ) );
     }
+}
+
+
+function displayResult( msg ){
+    if ( msg.endsWith( "win!!!" ) ) {
+        ++wins
+    } else if ( msg.endsWith( ":(" ) ) {
+        ++losses
+    } else {
+        ++ties 
+    }
+    if ( wins > 4 ) {
+        // DISPLAY USER WON 5 GAMES
+        elDisplayHeading.innerText = `You've won 5 games!  Out of ${wins + losses + ties}`;
+        wins = 0;
+        losses = 0;
+        ties = 0;
+    } else if ( losses > 4 ) {
+        // DISPLAY USER HAS LOST 5 GAMES
+        elDisplayHeading.innerText = `You've lost 5 games!  Out of ${wins + losses + ties}`;
+        wins = 0;
+        losses = 0;
+        ties = 0;
+    } else {
+        // DISPLAY the current standings and the last rounds result
+        elDisplayHeading.innerText = `${msg} \n Wins: ${wins}  Losses: ${losses}  Ties: ${ties}`;
+    }
+}
+
+
+function resetGame() {
+    wins = 0;
+    losses = 0;
+    ties = 0;
+    // <h1> = "Pick a choice to start the game.";
+    elDisplayHeading.innerText = "Pick a choice to start the game.";
 }
 
 
@@ -32,17 +74,7 @@ function capitilize (string){
     return firstLetter + string
 }
 
+const gameChoices = document.querySelectorAll("#game-choices button");
 
-winCount = 0;
-for (let i = 0; i < 5; i++) {
-
-    const PLAYERCHOICE = window.prompt( "Rock, Paper or Scissors?" );
-    const ROUND = oneRound( PLAYERCHOICE );
-    if ( ROUND === "Scissors beats Paper, you win!!!" || ROUND === "Paper beats Rock, you win!!!" ) {
-        console.log( ROUND );
-        winCount++
-    } else {
-        console.log( ROUND )
-    }
-};
-// console.log( "You've won "  )
+// used an anonymous function so I could pass in the buttons text as an argument
+gameChoices.forEach( choice => choice.addEventListener( "click", () => { oneRound( choice.innerText ); }, false) );
